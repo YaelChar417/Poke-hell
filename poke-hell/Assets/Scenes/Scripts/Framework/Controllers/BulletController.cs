32bcc9958d8505d32f03abc396e5316cb9810942
YaelChar417 <A01711111@tec.mx>
2025-08-26 22:32:47 -0600

    Feature: script para mover una bala y que desaparezca si choca

---
+++ poke-hell/Assets/Scenes/Scripts/Framework/Controllers/BulletController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private float speed = Constants.BULLET_SPEED;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveForward();
+    }
+
+    void MoveForward()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- poke-hell/Assets/Scenes/Scripts/Utils/Constants.cs
+++ poke-hell/Assets/Scenes/Scripts/Utils/Constants.cs
@@ -1,4 +1,5 @@
 public static class Constants
 {
     public const float SCENARIO_SPEED = 5.0f;
+    public const float BULLET_SPEED = 5.0f;
 }
