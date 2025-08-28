9ce2443482ad6bcabc9c4c89c46bbdf8094878f2
YaelChar417 <A01711111@tec.mx>
2025-08-28 11:16:55 -0600

    Update: script de corrutina del enemigo hecho modular

--- poke-hell/Assets/Scenes/Scripts/Framework/Controllers/EnemyController.cs
+++ poke-hell/Assets/Scenes/Scripts/Framework/Controllers/EnemyController.cs
@@ -15,28 +15,58 @@ public class EnemyController : MonoBehaviour
 
     private void TimeCheck()
     {
-        if(TimeManager.Hour == 10 && TimeManager.Minute == 2)
+        if(isTime(10, 2))
         {
-            StartCoroutine(MoveEnemy());
+            StartCoroutine(MoveEnemy(1));
+        } else if(isTime(10, 10))
+        {
+            StartCoroutine(MoveEnemy(2));
         }
+
         
     }
 
-    private IEnumerator MoveEnemy()
+    private IEnumerator MoveEnemy(int order)
     {
-        transform.position = new Vector3(-27.5f,3, -10);
-        Vector3 targetPos = new Vector3(-15.5f,3, -10);
-
-        Vector3 currentPos = transform.position;
-
         float timeElapsed = 0;
         float timeToMove = 3;
+        Vector3 targetPos;
+        Vector3 currentPos;
+        
+        switch(order)
+        {
+            case 1:
+                transform.position = new Vector3(-27.5f,3, -10);
+                targetPos = new Vector3(-15.5f,3, -10);
 
-        while(timeElapsed < timeToMove){
-            transform.position = Vector3.Lerp(currentPos,targetPos,timeElapsed/timeToMove);
-            timeElapsed += Time.deltaTime;
-            yield return null;
+                currentPos = transform.position;
+
+                while(timeElapsed < timeToMove){
+                    transform.position = Vector3.Lerp(currentPos,targetPos,timeElapsed/timeToMove);
+                    timeElapsed += Time.deltaTime;
+                    yield return null;
+                }
+                break;
+            case 2:
+                transform.position = new Vector3(-15.5f,3, -10);
+                targetPos = new Vector3(-27.5f,3, -10);
+
+                currentPos = transform.position;
+
+                while(timeElapsed < timeToMove){
+                    transform.position = Vector3.Lerp(currentPos,targetPos,timeElapsed/timeToMove);
+                    timeElapsed += Time.deltaTime;
+                    yield return null;
+                }
+                break;
+            default: 
+                yield return null;
+                break;
         }
+    }
 
+    private bool isTime(int hour, int minute)
+    {
+        return (TimeManager.Hour == hour && TimeManager.Minute == minute);
     }
 }
