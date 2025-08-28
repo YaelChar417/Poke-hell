8dcedc9f11809eaa6c5e487d367257b86f6b41f0
YaelChar417 <A01711111@tec.mx>
2025-08-28 16:34:57 -0600

    Feature: se agrego movimiento circular para el futuro segundo movimiento

--- poke-hell/Assets/Scenes/Scripts/Framework/Controllers/EnemyController.cs
+++ poke-hell/Assets/Scenes/Scripts/Framework/Controllers/EnemyController.cs
@@ -27,15 +27,36 @@ public class EnemyController : MonoBehaviour
         } else if(isTime(10, 14))
         {
             StartCoroutine(MoveEnemy(3));
+        } else if(isTime(10, 18))
+        {
+            StartCoroutine(MoveEnemy(5));
+        }else if(isTime(10, 20))
+        {
+            StartCoroutine(RotateEnemy());
         }
+    }
 
-        
+    private IEnumerator RotateEnemy()
+    {
+        float timeElapsed = 0f;
+        float timeToMove = 14.4f;
+        float rotationSpeed = 200.0f;
+        Vector3 center = new Vector3(-20.0f, 0f, -10);
+        Quaternion originalRotation = transform.rotation;
+
+        while(timeElapsed < timeToMove)
+        {
+            transform.RotateAround(center, Vector3.forward, rotationSpeed * Time.deltaTime);
+            transform.rotation = originalRotation;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
     }
 
     private IEnumerator MoveEnemy(int order)
     {
-        float timeElapsed = 0;
-        float timeToMove = 1;
+        float timeElapsed = 0f;
+        float timeToMove = 1.0f;
         Vector3 targetPos;
         Vector3 currentPos;
 
@@ -62,6 +83,12 @@ public class EnemyController : MonoBehaviour
             case 4: // derecha al centro
                 transform.position = new Vector3(-15.5f, 3.0f, -10);
                 targetPos = new Vector3(-20.0f, 4.0f, -10);
+
+                currentPos = transform.position;
+                break;
+            case 5: // derecha al centro en x, y
+                transform.position = new Vector3(-15.5f, 3.0f, -10);
+                targetPos = new Vector3(-20.0f, 2.0f, -10);
 
                 currentPos = transform.position;
                 break;
