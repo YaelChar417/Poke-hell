85d12d8bd4554afacce457202e277d52a5dadab6
YaelChar417 <A01711111@tec.mx>
2025-08-29 15:36:21 -0600

    Documentation: script utils/Constants documentado

--- poke-hell/Assets/Scenes/Scripts/Utils/Constants.cs
+++ poke-hell/Assets/Scenes/Scripts/Utils/Constants.cs
@@ -1,3 +1,14 @@
+/// <summary>
+/// La clase estatica Constants asegura que los valores declarados en ella sean
+/// accedibles para los demás scripts, en ella se definen los siguientes valores
+/// SCENARIO_SPEED -> float: velocidad a la que se mueve el escenario
+/// BULLET_SPEED -> float: velocidad a la que se mueven las balas
+/// BULLET_LIFE -> float: tiempo de vida de las balas antes de desaparecer
+/// RADIUS -> float: radio de un circulo
+/// 
+/// Standar coding documentation can be found in 
+/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+/// </summary>
 public static class Constants
 {
     public const float SCENARIO_SPEED = 3.0f;
