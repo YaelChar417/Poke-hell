1d87099c3ceb84f03be259a0a85ada5220c4d24a
YaelChar417 <A01711111@tec.mx>
2025-08-29 15:58:35 -0600

    Documentation: script de controller/BulletController documentado

--- poke-hell/Assets/Scenes/Scripts/Framework/Controllers/BulletController.cs
+++ poke-hell/Assets/Scenes/Scripts/Framework/Controllers/BulletController.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+/// <summary>
+/// La clase BulletController controla el comportamiento de cada bala en el 
+/// juego, desde la velocidad, direccion e incluso el tiempo en que esta activa 
+/// en el escenario
+/// 
+/// Standar coding documentation can be found in 
+/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+/// </summary>
 public class BulletController : MonoBehaviour
 {
     private float speed = Constants.BULLET_SPEED;
@@ -7,17 +15,30 @@ public class BulletController : MonoBehaviour
     private float bulletLife = Constants.BULLET_LIFE;
     public Vector2 direction = Vector2.down;
 
+    /// <summary>
+    /// OnEnable es llamado cuando se activa un objeto en unity, en este caso 
+    /// llama al metodo de BulletManager par sumar una bala
+    /// </summary>
     void OnEnable()
     {
         BulletManager.Add();
     }
 
+    /// <summary>
+    /// El metodo OnDisable es llamado cuando se desactiva un objeto, en este 
+    /// caso llama al metodo de BulletManager para restar una bala
+    /// </summary>
     void OnDisable()
     {
         BulletManager.Substract();
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// El metodo Update es llamado cada nuevo frame, verifica si el tiempo que
+    /// ha estado una bala es mayor que el tiempo de vida, si lo es destruye el 
+    /// objeto de bala, sino aumenta su tiempo a la cantidad de tiempo que paso 
+    /// entre frames y mueve la bala hacia adelante
+    /// </summary>
     void Update()
     {   
         if (timeCount > bulletLife)
@@ -29,6 +50,11 @@ public class BulletController : MonoBehaviour
         MoveForward();
     }
 
+    /// <summary>
+    /// El metodo MoveForward mueve a la bala en una direccion, en este caso por
+    /// defecto es hacia abajo y lo hace con la velocidad y el numero de frames
+    /// para que se mueva de igual forma en cada equipo
+    /// </summary>
     void MoveForward()
     {
         transform.Translate(direction.normalized * Time.deltaTime * speed);
